6caea62f918a01b078995dea63c6552828392738
FuneralMist <Andry1294@gmail.com>
2018-11-01 13:26:20 +0200

    dupa

--- src/main/java/epam/springbootmainproject/controllers/MainController.java
+++ src/main/java/epam/springbootmainproject/controllers/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -48,6 +49,12 @@ public class MainController {
     @GetMapping("/createNewCityButton")
     public String createNewCityButton() {
         return "cities/create";
+    }
+    
+    @GetMapping("/view")
+    public String resolveSingleContact(){
+
+        return "countries/view";
     }
 
 
